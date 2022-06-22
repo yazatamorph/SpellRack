3f96da409678edecd8abce7963e350f7dc2de894
Zareen Price <yazatamorph@gmail.com>
2022-06-22 09:31:35 -0600

    Continuing to build auth

--- server/src/main/java/com/spellrack/server/ServerApplication.java
+++ server/src/main/java/com/spellrack/server/ServerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.spellrack.server.model.Role;
 import com.spellrack.server.model.User;
@@ -19,18 +21,23 @@ public class ServerApplication {
 	}
 
 	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
 	CommandLineRunner run(UserService userService) {
 		return args -> {
-			userService.saveRole(new Role(null, "ROLL_USER"));
-			userService.saveRole(new Role(null, "ROLL_MANAGER"));
-			userService.saveRole(new Role(null, "ROLL_ADMIN"));
-			userService.saveRole(new Role(null, "ROLL_SUPER"));
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER"));
 
 			userService
 					.saveUser(new User(null, "zareensuxx", "zareensuxx@gmail.com", "beepboopboop", new ArrayList<>()));
 
-			userService.addRole("zareensuxx", "ROLL_USER");
-			userService.addRole("zareensuxx", "ROLL_SUPER");
+			userService.addRole("zareensuxx", "ROLE_USER");
+			userService.addRole("zareensuxx", "ROLE_SUPER");
 		};
 	}
 
--- server/src/main/java/com/spellrack/server/service/UserImplementation.java
+++ server/src/main/java/com/spellrack/server/service/UserImplementation.java
@@ -1,7 +1,13 @@
 package com.spellrack.server.service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,9 +21,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 @Transactional
-public class UserImplementation implements UserService {
+public class UserImplementation implements UserService, UserDetailsService {
     private final UserRepository userRepo;
     private final RoleRepository roleRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found.");
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                authorities);
+    }
 
     @Override
     public User saveUser(User user) {
